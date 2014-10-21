c5d6ae7b554b087dd07d1df2b22df986ed1c437d
Mark Donnelly <mark@painless-security.com>
2014-10-20 22:09:01 -0400

    Update the tester page to add init_sec_context and acquire_cred spaces.
    * Change the callback signature from the response dispatcher
    * Change the tester page to add space for acquire cred
    * Change the tester page to add space for init sec context
    * Fill in the GSS names underneath acquire_cred and
      init_sec_context upon successful gss name importing

--- chrome/app/navigator.gss.js
+++ chrome/app/navigator.gss.js
@@ -68,7 +68,7 @@ var GSSEap = (function ()
               app_tag);
         } else {
             if ("gss_import_name" == method) 
-            { callback(event.data.return_values.gss_name, app_tag); }
+            { callback(event.data.return_values, app_tag); }
             // if( "gss_X" == method ) { callback(event.data.return_values.x, app_tag);}
         }
     };
