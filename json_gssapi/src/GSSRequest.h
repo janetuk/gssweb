3d0650424848f02b1e3d27aff4343b28c70b0c48
Kevin Wasserman <krwasserman@hotmail.com>
2014-11-06 21:47:00 -0500

    Support for windows builds

--- json_gssapi/src/GSSRequest.h
+++ json_gssapi/src/GSSRequest.h
@@ -9,7 +9,7 @@
 #define GSSREQUEST_H
 
 #include <string>
-
+#include "jsongssapi_export.h"
 #include "commands/GSSCommand.h"
 #include "util_json.h"
 
@@ -18,11 +18,11 @@ using std::string;
 class GSSRequest
 {
 public:
-  GSSRequest(string jsonString);
-  ~GSSRequest();
+  JSONGSSAPI_EXPORT GSSRequest(string jsonString);
+  JSONGSSAPI_EXPORT ~GSSRequest();
   
-  void execute();
-  string getResponse();
+  JSONGSSAPI_EXPORT void execute();
+  JSONGSSAPI_EXPORT string getResponse();
   
 private:
   string      requestString;
