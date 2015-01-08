df68651046d357c55a1202042e52aa44ecfa2d41
Kevin Wasserman <krwasserman@hotmail.com>
2015-01-08 16:59:09 -0500

    target x86, not x64 in win-build.bat

--- win-build.bat
+++ win-build.bat
@@ -3,6 +3,6 @@ del /f /s /q build
 rd /s /q build
 mkdir build
 cd build
-call setenv /x64 /Debug
+call setenv /x86 /Debug
 cmake -DCMAKE_BUILD_TYPE=Debug -G "NMake Makefiles" ../
 nmake -f Makefile
