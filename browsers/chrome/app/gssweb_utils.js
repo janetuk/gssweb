069d76797b7a1686356140993aef0d498aeac67a
Margaret Wasserman <mrw@painless-security.com>
2015-04-02 11:25:49 -0400

    Remove duplicated navigator js file from chrome/app directory.

--- browsers/chrome/app/gssweb_utils.js
+++
@@ -1,9 +0,0 @@
-console.log('Loading gssweb_utils.js - #1');
-
-navigator.generateNonce = function() {
-  // TODO: Make sure that we don't have a collision!
-  // Random integer in the range [0..(2^32-1)]
-  return Math.floor(Math.random() * ( 4294967295 )) ;
-}
-
-
